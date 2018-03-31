6b674511c71e7ee985fd6e549d00478a918e6833
Niraj Trivedi <mailme7778@gmail.com>
2018-04-01 01:27:13 +1100

    Updated unit of work

---
+++ Service/UnitOfWork.cs
@@ -0,0 +1,63 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Security.Permissions;
+using Entity;
+
+namespace Service
+{
+    public class UnitOfWork : IDisposable
+    {
+        public readonly EntityDbContext _context;
+        private bool disposed;
+        private Dictionary<string, object> _repositories;
+
+        public UnitOfWork(EntityDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(true);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+
+            disposed = true;
+        }
+
+        public Repository<T> Repository<T>() where T : BaseEntity
+        {
+            if (_repositories == null)
+            {
+                _repositories = new Dictionary<string, object>();
+            }
+
+            var type = typeof(T).Name;
+            if (!_repositories.ContainsKey(type))
+            {
+                var repositoryType = typeof(Repository<T>);
+                var repositoryInstance = Activator.CreateInstance(repositoryType.MakeGenericType(typeof(T)), _context);
+                _repositories.Add(type, repositoryInstance);
+            }
+
+            return (Repository<T>)_repositories[type];
+        }
+    }
+}

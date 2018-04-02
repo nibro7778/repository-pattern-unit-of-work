cf46fbadcea0d60f2c55fb01a9f558fcaf19d9c8
Niraj Trivedi <mailme7778@gmail.com>
2018-04-02 15:03:52 +1000

    Added Test Project

---
+++ WebApp.Test/HelloWorldTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace WebApp.Test
+{
+    [TestFixture]
+    public class HelloWorldTest
+    {
+        [Test]
+        public void String_must_be_same()
+        {
+            const int sum = 5;
+            Assert.AreEqual(2 + 3,sum);
+        }
+    }
+}
---
+++ WebApp.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WebApp.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebApp.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1d968de7-f1c9-432d-bf8d-6aa9a78bfc48")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
